1b2e3ab95fb566a76db566babe4da7768dc831b1
E0ho <leeyeongho3404@gmail.com>
2024-07-12 20:34:45 +0900

    feat: interactive chatbot (대화형) 기능 추가

--- backend/chatbot/urls.py
+++ backend/chatbot/urls.py
@@ -5,4 +5,6 @@ app_name = 'chatbot'
 
 urlpatterns = [
     path("simple/", views.simple_chatbot, name='simple'),         # 단순형 Chatbot
+    path("interactive/", views.interactive_chatbot, name='interactive'),         # 단순형 Chatbot
 ]
+
--- backend/chatbot/views.py
+++ backend/chatbot/views.py
@@ -51,3 +51,56 @@ def simple_chatbot(request):
 
 
 
+#################### 2. 대화형 chatbot #################### 
+def interactive_chatbot(request):
+    
+    global memory
+
+    # URL 페이지 방문
+    if request.method == 'GET':
+
+        # Memory 초기화 (처음에만 대화 초기화)
+        memory = new_memory()
+        
+        # 입력 Form
+        form = HisotryModelForm()
+        
+        # 대화 흐름 세션에서 가져오기
+        chat_history = request.session.get('chat_history',[])
+        
+        context = {
+            'form':form,
+            'chat_history': chat_history,
+        }
+
+        return render(request, 'chatbot/interactive_chatbot.html', context)
+    
+    else:
+        
+        # chatbot 생성 (대화 흐름 기억)
+        chatbot = rag_gpt_chatbot(memory)
+        
+        # 질의 가져오기
+        query = request.POST.get('question')
+        answer = chatbot(query)                         # Chatbot 답변 생성
+        
+        # 대화 흐름 세션에서 가져와서 (질의 - 응답) 추가
+        chat_history = request.session.get('chat_history',[])
+        chat_history.append({'question': query, 'answer': answer['answer']})
+        request.session['chat_history'] = chat_history
+        
+        context = {
+            'chat_history': chat_history
+        }
+    
+        # History 모델에 저장하기
+        history = History()
+        history.question = query
+        history.answer = answer['answer']
+        history.save()
+        
+        return render(request, 'chatbot/interactive_chatbot.html', context)
+
+
+
+
