05f8f432e0b3c6d1c2db979a2a19e64b388f748f
E0ho <leeyeongho3404@gmail.com>
2024-07-12 22:47:33 +0900

    refactor: Chatbot URL 경로 설정

--- backend/chatbot/urls.py
+++ backend/chatbot/urls.py
@@ -4,7 +4,9 @@ from . import views
 app_name = 'chatbot'
 
 urlpatterns = [
-    path("simple/", views.simple_chatbot, name='simple'),         # 단순형 Chatbot
-    path("interactive/", views.interactive_chatbot, name='interactive'),         # 단순형 Chatbot
+    path("simple/", views.simple_chatbot, name='simple'),                  # 단순형 Chatbot
+    path("interactive/", views.interactive_chatbot, name='interactive'),   # 대화형 Chatbot
+    path("custom/", views.custom_chatbot, name='custom'),                  # 커스텀 Chatbot
+    path('add_vectorDB/', views.add_vectorDB, name='add_vectorDB')
 ]
 
