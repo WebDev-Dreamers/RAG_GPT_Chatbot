74fe2bf12747682db07c02839368c323a39eba79
E0ho <leeyeongho3404@gmail.com>
2024-07-12 20:24:30 +0900

    feat: 대화 History 저장 Model

---
+++ backend/chatbot/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+from .models import History
+
+# History 입력 ModelForm
+class HisotryModelForm(forms.ModelForm):
+    class Meta:
+        model = History
+        fields = ['question']
+                  
+        labels = {
+            'question': '',
+            'answer': 'Your Answer',
+        }
--- backend/chatbot/models.py
+++ backend/chatbot/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+# 대화를 기억할 History 모델 선언
+class History(models.Model):
+    datetime = models.DateTimeField(auto_now_add=True)
+    question = models.TextField()
+    answer = models.TextField()
+    
+    def __str__(self):
+        return f"Date: {self.datetime}     Q: {self.question}    Answer: {self.answer}"
+    
+    class Meta:
+        db_table = 'history'
